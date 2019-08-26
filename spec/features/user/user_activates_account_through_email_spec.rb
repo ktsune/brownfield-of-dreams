8d21e233a27e618b8834f9231da6b179d7e67181
Sarah Tatro <statrox4@gmail.com>
2019-08-26 16:10:59 -0600

    Create test for user activating account through email

---
+++ spec/features/user/user_activates_account_through_email_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User Email' do
+  scenario 'user can activate their account through email' do
+    user = User.create(first_name: 'Aurie', last_name: 'Auriest', password: 'password')
+
+    visit '/'
+    click_on 'Register'
+
+    expect(current_path).to eq('/register')
+
+    fill_in :first_name, with: 'Aurie'
+    fill_in :last_name, with: 'Auriest'
+    fill_in :password, with: 'password'
+    fill_in :password_confirmation, with: 'password'
+    click_on 'Submit'
+
+    expect(current_path).to eq('/dashboard')
+    expect(page).to have_content("Logged in as #{user.first_name}")
+    expect(page).to have_content("This account has not yet been activated. Please check your email.")
+  end
+end
