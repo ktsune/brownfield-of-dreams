dfb30f7d1c3a4638a2ca4311621594d9c27c92aa
Sarah Tatro <statrox4@gmail.com>
2019-08-26 16:59:27 -0600

    Call the mailer upon user creation
    Co-Authored-By: Aurie <Myrdden@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     user = User.create(user_params)
     if user.save
+      EmailActivationMailer.activate(user).deliver_now
       session[:user_id] = user.id
       redirect_to dashboard_path
     else
