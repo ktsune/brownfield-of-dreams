b8d909b1b1c63a75365f54eecff181f6a69362b3
Sarah Tatro <statrox4@gmail.com>
2019-08-26 15:37:54 -0600

    Refactor Classroom Content Check Into Tutorial Model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,17 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    if current_user
-      if params[:tag]
-        @tutorials = Tutorial.tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-      else
-        @tutorials = Tutorial.all.paginate(:page => params[:page], :per_page => 5)
-      end
-    else 
-      if params[:tag]
-        @tutorials = Tutorial.where(classroom: false).tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
-      else
-        @tutorials = Tutorial.where(classroom: false).paginate(:page => params[:page], :per_page => 5)
-      end
+    if params[:tag]
+      @tutorials = Tutorial.classroom?(current_user).tagged_with(params[:tag]).paginate(:page => params[:page], :per_page => 5)
+    else
+      @tutorials = Tutorial.classroom?(current_user).paginate(:page => params[:page], :per_page => 5)
     end
   end
 end
