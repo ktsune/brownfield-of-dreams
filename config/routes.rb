3a8cc0a271e2567e642ec4a87f92ce3477ef64d7
Aurie <Myrdden42@gmail.com>
2019-08-27 15:11:58 -0600

    Organise Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,34 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'tags/:tag', to: 'welcome#index', as: :tag
+
+  get '/about', to: 'about#show'
+  get '/get_started', to: 'get_started#show'
+
+  # Users
+  get '/register', to: 'users#new'
+  get '/dashboard', to: 'users#show'
+  resources :users, only: %i[create update edit]
+  post '/friend', to: 'friend#create', as: 'create_friend'
+
+  # Sessions
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/auth/github', as: 'github_login'
+  get '/auth/github/callback', to: 'sessions#update'
+  delete '/logout', to: 'sessions#destroy'
+
+  # Bookmarks
+  resources :user_videos, only: %i[create destroy]
+
+  # Tutorials
+  resources :tutorials, only: %i[show index] do
+    resources :videos, only: %i[show index]
+  end
+
+  # API
   namespace :api do
     namespace :v1 do
       resources :tutorials, only: %i[show index]
@@ -8,12 +36,10 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'welcome#index'
-  get 'tags/:tag', to: 'welcome#index', as: :tag
-  get '/register', to: 'users#new'
-
+  # Admin
   namespace :admin do
     get '/dashboard', to: 'dashboard#show'
+
     resources :tutorials, only: %i[create edit update destroy new] do
       resources :videos, only: [:create]
     end
@@ -25,28 +51,4 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  post '/friend', to: 'friend#create', as: 'create_friend'
-
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-
-  get '/auth/github', as: 'github_login'
-  get '/auth/github/callback', to: 'sessions#update'
-
-  get '/dashboard', to: 'users#show'
-  get '/about', to: 'about#show'
-  get '/get_started', to: 'get_started#show'
-
-  # Is this being used?
-  get '/video', to: 'video#show'
-
-  resources :users, only: %i[new create update edit]
-
-  resources :tutorials, only: %i[show index] do
-    resources :videos, only: %i[show index]
-  end
-
-  resources :user_videos, only: %i[create destroy]
 end
