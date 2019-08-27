9aa2322959719a283afde9ae42454300ef120354
Sarah Tatro <statrox4@gmail.com>
2019-08-27 14:26:18 -0600

    email changes

--- config/environment.rb
+++ config/environment.rb
@@ -9,3 +9,13 @@ Rails.application.initialize!
 Rails.application.configure do
   config.action_mailer.delivery_method = :test
 end
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
