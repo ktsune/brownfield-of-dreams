67cccd6c66dbd3f0019b5028ed18e8e01422b7bb
Aurie <Myrdden42@gmail.com>
2019-08-28 13:42:26 -0600

    Add Invite to Mailer

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -4,7 +4,7 @@ class InviteController < ApplicationController
   def create
     email = GithubService.new(current_user).fetch_email(params[:handle])
     if email
-      UserMailer.invite(current_user, params[:handle]).deliver_now
+      UserMailer.invite(current_user, params[:handle], email).deliver_now
       flash[:success] = "Sent an invite to #{params[:handle]}"
     else
       flash[:error] = "#{params[:handle]} doesn\'t have an email associated with their account."
