7c4b451e5fb83c1cf8661a989423a777268b4cd8
Aurie <Myrdden42@gmail.com>
2019-08-28 13:33:44 -0600

    Add User Invite Logic

---
+++ app/controllers/invite_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class InviteController < ApplicationController
+  def create
+    email = GithubService.new(current_user).fetch_email(params[:handle])
+    if email
+      UserMailer.invite(current_user, params[:handle]).deliver_now
+      flash[:success] = "Sent an invite to #{params[:handle]}"
+    else
+      flash[:error] = "#{params[:handle]} doesn\'t have an email associated with their account."
+    end
+    redirect_to dashboard_path
+  end
+end
