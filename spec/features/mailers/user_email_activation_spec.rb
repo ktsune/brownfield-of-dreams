89c80f75de6f79b92547e1a7305654803b5ebfe9
Aurie <Myrdden42@gmail.com>
2019-08-28 14:09:22 -0600

    Add Invite Mailer Spec

--- spec/features/mailers/user_email_activation_spec.rb
+++ spec/features/mailers/user_email_activation_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe UserMailer, type: :mailer do
-  describe 'instructions' do
-
-    let(:user) { mock_model User, name: 'Aurie', email: 'aurie@email.com' }
-    let(:mail) { described_class.instructions(user).deliver_now }
+  describe 'activate' do
+    let(:user) { create(:user, uuid: '13579') }
+    let(:mail) { described_class.activate(user).deliver_now }
 
     it 'renders the subject' do
       expect(mail.subject).to eq('Activate your account.')
@@ -20,7 +19,29 @@ RSpec.describe UserMailer, type: :mailer do
 
     it 'assigns @confirmation_url' do
       expect(mail.body.encoded)
-        .to match("http://localhost:3000/activate?user_id=#{user.id}")
+        .to match("http://localhost:3000/activate?uuid=#{user.uuid}")
+    end
+  end
+
+  describe 'invite' do
+    let(:user) { create(:user) }
+    let(:handle) { 'Mr. Handle' }
+    let(:email) { 'handle@github.com' }
+    let(:mail) { described_class.invite(user, handle, email).deliver_now }
+
+    it 'renders subject' do
+      expect(mail.subject).to eq("#{user.first_name} invited you to Turing Tutorials.")
+    end
+
+    it 'renders emails' do
+      expect(mail.from).to eq(['noreply@turingtutorials.com'])
+      expect(mail.to).to eq([email])
+    end
+
+    it 'renders body' do
+      expect(mail.body.encoded).to match('Hello Mr. Handle!')
+      expect(mail.body.encoded).to match("#{user.first_name} has invited you to join Turing Tutorials.")
+      expect(mail.body.encoded).to match('/register')
     end
   end
 end
